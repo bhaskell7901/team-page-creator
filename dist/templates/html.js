32f1a8aa1d3623f290adddc039128b8702224152
Brandon Haskell <bdh747flyna1@hotmail.com>
2023-01-25 23:57:17 -0800

    add getHtmlCard prototype function for fun

--- dist/templates/html.js
+++ dist/templates/html.js
@@ -7,12 +7,12 @@ function getHtmlPage(teamName, managerCards, engineerCards, internCards) {
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"/>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-        <title>${team.teamName}</title>
+        <title>${teamName}</title>
         <style type="text/css"></style>
     </head>
     <body style="background-color: #dfdfdf;">
         <header>
-            <h1 class="display-1 fw-bold text-center text-bg-dark">Team One</h1>
+            <h1 class="display-1 fw-bold text-center text-bg-dark">${teamName}</h1>
         </header>
         <main id="container" class="container-sm">
             <div id="manager" class="row align-items-center">
@@ -35,7 +35,7 @@ function getHtmlPage(teamName, managerCards, engineerCards, internCards) {
             <!-- End Intern list --></div>
 
             </div>
-                    </main>
+        </main>
     </body>
 </html>`;
 }
--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-
+const Employee = require('./lib/classes/Employee');
 const Manager = require('./lib/classes/Manager');
 const Engineer = require('./lib/classes/Engineer');
 const Intern = require('./lib/classes/Intern');
+const getHtmlPage = require('./dist/templates/html');
 
 const fs = require('fs');
+const questions = require('./lib/utils/questions');
 const inquirer = require('inquirer');
 inquirer.registerPrompt("loop", require("inquirer-loop")(inquirer))
 
 function getTeamAnswers(){
 
     inquirer
-        .prompt()
+        .prompt(questions)
         .then((data) => {
             console.log(data);
         })
@@ -23,4 +25,10 @@ function getTeamAnswers(){
         });
 }
 
-getTeamAnswers();
+// getTeamAnswers();
+
+test1 = new Manager("Brandon Haskell", 1001, "gmail@gmail.com", "30F");
+test2 = new Engineer("Franklin Roosevelt", 1002, "frank@gmail.com", "bhaskell7901");
+test3 = new Intern("Sarah Berryman", 1003, "saber@gmail.com", "Univerisyt of North Dakota");
+
+console.log(getHtmlPage("Team Name Here", test1.getHtmlCard(), test2.getHtmlCard(), test3.getHtmlCard()));
